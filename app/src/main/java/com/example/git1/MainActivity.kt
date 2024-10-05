9f637090c4bdf01b67833ab4a4dc933fca3cf6b0
sikander11031 <sikanderahmad1103@gmail.com>
2024-10-05 18:10:15 +0500

    name change

--- app/src/main/java/com/example/git1/MainActivity.kt
+++ app/src/main/java/com/example/git1/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Git1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "sikander",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
